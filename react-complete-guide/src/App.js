9b4abba88feae313617f7875166914ae3eef906c
Sandhya Kumari <sandhyakumari@Sandhyas-MacBook-Pro.local>
2021-11-28 12:36:38 -0500

    Adding new user input form

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App">      
+      <NewExpense/>
       <Expenses></Expenses>
     </div>
   );
