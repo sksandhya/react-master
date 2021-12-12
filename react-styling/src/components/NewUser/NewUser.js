61b29ec03d5e5b13dd8500947c3ca73afea32933
Sandhya Kumari <sandhyakumari@Sandhyas-MacBook-Pro.local>
2021-12-12 18:50:30 -0500

    Another commits

---
+++ react-styling/src/components/NewUser/NewUser.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NewUser = () =>{
+
+    return(<div>
+ <label>Add User</label>
+        <input type="text"/>    
+        <label>Add Age</label>
+        <input type="text"/>
+        <button type="submit">Add User</button>
+    </div>);
+};
+
+export default NewUser;
